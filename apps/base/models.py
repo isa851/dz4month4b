1dda135de2f79804a887b9ba50bc1046e318a2bf
isa851 <isa904363@gmail.com>
2026-01-17 13:40:29 +0600

    fix

--- apps/base/models.py
+++ apps/base/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 class Base(models.Model):
     title = models.CharField(max_length=255,verbose_name='Название')
+    description = models.TextField(verbose_name='описание')
+    phone = models.CharField(max_length=255,verbose_name='номер')
+    adres = models.CharField(max_length=255,verbose_name='адрес')
+    logo = models.ImageField(upload_to = "base/", verbose_name = "Логотив")
+    
     class Meta:
         verbose_name = 'Базовый'
         verbose_name_plural = 'Базовые'
+        
--- core/urls.py
+++ core/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from apps.base.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", index, name = "index-page"),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
